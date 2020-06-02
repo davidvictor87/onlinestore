c7ab7c8141558b50440747878524ab902b080eab
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-06-02 17:39:01 +0300

    Handle Access

--- src/main/java/spring/online/store/security/configuration/HandleAccess.java
+++ src/main/java/spring/online/store/security/configuration/HandleAccess.java
@@ -1,8 +1,19 @@
 package spring.online.store.security.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class HandleAccess {
+	
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http.authorizeExchange().pathMatchers(HttpMethod.GET).permitAll()
+		.pathMatchers("/login", "/welcome").permitAll();
+		return http.build();
+	}
 
 }
