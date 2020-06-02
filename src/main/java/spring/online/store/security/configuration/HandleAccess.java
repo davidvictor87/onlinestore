14ad326a898662b468f714895ed830c0c815d6a7
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-06-02 17:04:12 +0300

    .

--- src/main/java/spring/online/store/security/configuration/HandleAccess.java
+++ src/main/java/spring/online/store/security/configuration/HandleAccess.java
@@ -1,5 +1,7 @@
 package spring.online.store.security.configuration;
 
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
 @EnableWebFluxSecurity
 public class HandleAccess {
 
