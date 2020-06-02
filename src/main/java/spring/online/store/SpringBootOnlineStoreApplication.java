ae5c92ecd41cf78e7e038419486293d3fca45667
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-06-02 13:35:24 +0300

    .

---
+++ src/main/java/spring/online/store/SpringBootOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package spring.online.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/online/store/SpringBootOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.online.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
