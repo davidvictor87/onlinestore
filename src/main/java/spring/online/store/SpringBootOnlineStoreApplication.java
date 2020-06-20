f5f5cedd6aacecc92a8b95387e603e2a6834ad26
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-06-20 09:06:27 +0300

    Fix Mongo db connection bug

--- src/main/java/spring/online/store/SpringBootOnlineStoreApplication.java
+++ src/main/java/spring/online/store/SpringBootOnlineStoreApplication.java
@@ -3,12 +3,13 @@ package spring.online.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude= {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ComponentScan({"spring.online.store.security.configuration", "spring.online.store.login.repository", "spring.online.store.login.model", "spring.online.store.db"})
 @EnableMongoRepositories
 public class SpringBootOnlineStoreApplication {
